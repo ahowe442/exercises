02b05aff1d2c99b0f42f0238e1b45af79ce11d7f
Abby H <howe.abigail@gmail.com>
2021-02-12 13:43:42 -0600

    Complete part 1 of express-session demo

--- sessionDemo/index.js
+++ sessionDemo/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+
+app.use(session({secret: 'thisisnotagoodsecret'}));
+
+app.get('/viewcount', (req, res)=> {
+    if (req.session.count){
+        req.session.count += 1; 
+    } 
+    else{
+        req.session.count = 1; 
+    }
+    res.send(`YOU HAVE VIEWED THIS PAGE ${req.session.count} TIMES.`)
+});
+
+/**
+ * The session is stored in memory. This is not what we would do for production.  It will leak memory under most conditions.  We will want to use ?? redisStore??  or ???mongo???
+ */
 
 app.listen(3000, ()=>{
     console.log('Listening on port 3000');
