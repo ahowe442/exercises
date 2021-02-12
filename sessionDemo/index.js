908e9990ee286af44f7d35c63cd398c1fe6a1708
Abby H <howe.abigail@gmail.com>
2021-02-12 15:49:47 -0600

    Add registration and greet session cookie information

--- sessionDemo/index.js
+++ sessionDemo/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const sessionOptions = {
+    secret: 'thisisnotagoodsecret',
+    resave: false,
+    saveUninitialized: false,
+}
 
-app.use(session({secret: 'thisisnotagoodsecret'}));
+app.use(session(sessionOptions));
 
 app.get('/viewcount', (req, res)=> {
     if (req.session.count){
@@ -17,6 +22,19 @@ app.get('/viewcount', (req, res)=> {
 /**
  * The session is stored in memory. This is not what we would do for production.  It will leak memory under most conditions.  We will want to use ?? redisStore??  or ???mongo???
  */
+
+
+ // This is used to demo that we can put additional things in our session. 
+ app.get('/register', (req,res)=> {
+     const {username = 'anonymous' } = req.query;
+     req.session.username = username; 
+     res.redirect('/greet');
+ });
+
+ app.get('/greet', (req,res)=>{
+     const { username } = req.session;
+     res.send(`Welcome Back, ${username}!`);
+ });
 
 app.listen(3000, ()=>{
     console.log('Listening on port 3000');
